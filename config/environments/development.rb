066355692097311245d0b20147d1cd41fa9ccbad
Enrique Quiroga <enrockq@gmail.com>
2013-05-30 00:56:06 -0500

    Removed index.html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,3 +35,5 @@ Omrails::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 end
+
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
